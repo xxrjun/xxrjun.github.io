4bc1e7526cc36d1fa79895c4d86dbe0559081b91
rjun <rjun0729@gmail.com>
2023-05-14 03:36:59 +0800

    feat: update AppMetadata.jsx

--- components/AppMetadata.jsx
+++ components/AppMetadata.jsx
@@ -1,43 +1,29 @@
 const author = "xxrjun";
-const description =
-	"A student who loves new technology and programming.";
+const description = "A student who loves new technology and programming.";
 const url = "";
 export const AppMetadata = {
-	title: {
-		default: `${author} | Portfolio`
-	},
-	description: description,
-	icons: {
-		icon: "/favicon.png"
-	},
-	keywords: [
-		"xxrjun",
-		"Portfolio website",
-		"React Portfolio",
-		"Next.js Portfolio",
-	],
-	creator: author,
-	authors: [{ name: author, url: url }],
-	openGraph: {
-		title: `${author} | Portfolio`,
-		description: description,
-		url: url,
-		siteName: `${author} | Portfolio`,
-		images: [
-			{
-				url: "https://vasile-novatchii.netlify.app/screenshot.webp",
-				width: 800,
-				height: 600,
-				alt: "My personal portfolio website"
-			},
-			{
-				url: "https://vasile-novatchii.netlify.app/screenshot.webp",
-				width: 1800,
-				height: 1600,
-				alt: "My personal portfolio website"
-			}
-		],
-		locale: "en-US",
-		type: "website"
-	}
+  title: {
+    default: `${author} | Portfolio`,
+  },
+  description: description,
+  icons: {
+    icon: "/favicon.png",
+  },
+  keywords: [
+    "xxrjun",
+    "Portfolio website",
+    "React Portfolio",
+    "Next.js Portfolio",
+  ],
+  creator: author,
+  authors: [{ name: author, url: url }],
+  openGraph: {
+    title: `${author} | Portfolio`,
+    description: description,
+    url: url,
+    siteName: `${author} | Portfolio`,
+
+    locale: "en-US",
+    type: "website",
+  },
 };
