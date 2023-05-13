433ccc1b0537417a700b439ca0080f211a518a1d
rjun <rjun0729@gmail.com>
2023-05-13 19:58:15 +0800

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('./public/assets/herobg.png')",
+        "hero-pattern": "url('/public/assets/herobg.png')",
       },
     },
   },
